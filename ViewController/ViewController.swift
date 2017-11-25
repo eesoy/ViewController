a7df0f099dbf39e45f9ae9c0e56644283e499adc
그린아카데미 <greengn@geulin-akademiui-iMac.local>
2017-11-25 19:25:23 +0900

    Initial Commit

---
+++ ViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewController
+//
+//  Created by 그린아카데미 on 2017. 11. 25..
+//  Copyright © 2017년 soyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
